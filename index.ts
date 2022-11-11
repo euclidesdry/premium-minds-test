07252fe019835bd51327acf39073bd6e1598332f
Euclides do Rosário <euclidesdry@gmail.com>
2022-11-11 12:47:29 +0100

    feat: 🎉 init project

---
+++ index.ts
@@ -0,0 +1,11 @@
+"use strict";
+
+function sayMyName(name: string): void {
+	if (name === "euclidesdry") {
+		console.log("You're right 👍");
+	} else {
+		console.log("You're wrong 👎");
+	}
+}
+
+sayMyName("euclidesdry");
