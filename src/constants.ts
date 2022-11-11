7cfa2900ba7791f9a573f9d6bd8a72cf6f046cea
Euclides do Rosário <euclidesdry@gmail.com>
2022-11-11 16:48:11 +0100

    refactor: ⚡️ updating system message constant

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,5 @@
-export const USER_INPUT_MSG =
-	"Por favor, insira as movimentações do Ash usando os pontos cardinais (N,S, E ou O): ";
+export enum sysMessage {
+	USER_INPUT = "Por favor, insira as movimentações do Ash usando os pontos cardinais (N,S, E ou O): ",
+	ERROR_INVALID_DIRECTIONS = "Você introduziu algum caractere que não pertence aos pontos cardiais (N,S,E ou O)",
+	ERROR_NO_INPUT = "Você não introduziu nenhuma movimentação para o Ash relativas aos pontos cardiais (N,S,E ou O)",
+}
