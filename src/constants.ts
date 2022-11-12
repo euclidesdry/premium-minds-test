9a62bf8ff8c7fb2cd393ce439159b3742d654119
Euclides do Rosário <euclidesdry@gmail.com>
2022-11-12 11:41:30 +0100

    feat: ✨ adding error message for under 5 given map range

--- src/constants.ts
+++ src/constants.ts
@@ -2,4 +2,5 @@ export enum sysMessage {
 	USER_INPUT = "Por favor, insira as movimentações do Ash usando os pontos cardinais (N,S, E ou O): ",
 	ERROR_INVALID_DIRECTIONS = "Você introduziu algum caractere que não pertence aos pontos cardiais (N,S,E ou O)",
 	ERROR_NO_INPUT = "Você não introduziu nenhuma movimentação para o Ash relativas aos pontos cardiais (N,S,E ou O)",
+	ERROR_UNDER_THAN_5 = "the map range must be greater or equal to 5",
 }
