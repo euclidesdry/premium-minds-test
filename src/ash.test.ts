bab2d53060ac453d7f6b11097ea634123f725890
Euclides do Rosário <euclidesdry@gmail.com>
2022-11-12 17:49:52 +0100

    test: ✅ adding mock to ash.test

--- src/ash.test.ts
+++ src/ash.test.ts
@@ -1,19 +1,27 @@
 import { catchPokemon } from "./ash";
 
+const MOCK_1ST_INPUT_EXAMPLE = "E";
+const MOCK_2ND_INPUT_EXAMPLE = "NESO";
+const MOCK_3RD_INPUT_EXAMPLE = "NSNSNSNSNS";
+
+const MOCK_1ST_RETURN_EXAMPLE = 2;
+const MOCK_2ND_RETURN_EXAMPLE = 4;
+const MOCK_3RD_RETURN_EXAMPLE = 2;
+
 describe("Ash", () => {
 	describe("catchPokemon function", () => {
-		it("should return 2", () => {
-			const expectedPokemon = catchPokemon("E");
+		it(`should return ${MOCK_1ST_RETURN_EXAMPLE} as the first Premium Minds example`, () => {
+			const expectedPokemon = catchPokemon(MOCK_1ST_INPUT_EXAMPLE);
 			expect(expectedPokemon).toBe(2);
 		});
 
-		it("should return 4", () => {
-			const expectedPokemon = catchPokemon("NESO");
+		it(`should return ${MOCK_2ND_RETURN_EXAMPLE} as the second Premium Minds example`, () => {
+			const expectedPokemon = catchPokemon(MOCK_2ND_INPUT_EXAMPLE);
 			expect(expectedPokemon).toBe(4);
 		});
 
-		it("should return 2", () => {
-			const expectedPokemon = catchPokemon("NSNSNSNSNS");
+		it(`should return ${MOCK_3RD_RETURN_EXAMPLE} as the third Premium Minds example`, () => {
+			const expectedPokemon = catchPokemon(MOCK_3RD_INPUT_EXAMPLE);
 			expect(expectedPokemon).toBe(2);
 		});
 	});
