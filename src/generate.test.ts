41258bbbc0f603ac33b6edab601dccbd9e9b350d
Euclides do Rosário <euclidesdry@gmail.com>
2022-11-12 11:42:13 +0100

    test: ✅ adding tests for generate.ts function file

---
+++ src/generate.test.ts
@@ -0,0 +1,33 @@
+import { sysMessage } from "./constants";
+import { generate2DWorld } from "./generate";
+
+const MOCK_SQUARE_MATRIX_NUM = 10;
+const MOCK_FAIL_SQUARE_MATRIX_NUM = 0;
+
+describe("generate2DWorld function", () => {
+	it(`should generate a square bidimensional matrix of 5 items`, () => {
+		const generated2DWorld = generate2DWorld();
+		const expected =
+			generated2DWorld.length === 5 && generated2DWorld.length === 5;
+
+		expect(expected).toBeTruthy();
+	});
+
+	it(`should generate a square bidimensional of given matrix of ${MOCK_SQUARE_MATRIX_NUM} items`, () => {
+		const generated2DWorld = generate2DWorld(MOCK_SQUARE_MATRIX_NUM);
+		const expected =
+			generated2DWorld.length === MOCK_SQUARE_MATRIX_NUM &&
+			generated2DWorld.length === MOCK_SQUARE_MATRIX_NUM;
+
+		expect(expected).toBeTruthy();
+	});
+
+	it(`should throw an error for value smaller than 5`, () => {
+		expect(() => {
+			generate2DWorld(MOCK_FAIL_SQUARE_MATRIX_NUM);
+		}).toThrow(Error);
+		expect(() => {
+			generate2DWorld(MOCK_FAIL_SQUARE_MATRIX_NUM);
+		}).toThrow(sysMessage.ERROR_UNDER_THAN_5);
+	});
+});
