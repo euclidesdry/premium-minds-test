9115f3c1dec6e1223ac8fd510e89a9549fb7d36a
Euclides do Rosário <euclidesdry@gmail.com>
2022-11-13 11:42:36 +0100

    test: ✅ updating with error handlers test

--- src/ash.test.ts
+++ src/ash.test.ts
@@ -1,8 +1,11 @@
 import { catchPokemon } from "./ash";
+import { sysMessage } from "./constants";
 
 const MOCK_1ST_INPUT_EXAMPLE = "E";
 const MOCK_2ND_INPUT_EXAMPLE = "NESO";
 const MOCK_3RD_INPUT_EXAMPLE = "NSNSNSNSNS";
+
+const MOCK_INVALID_INPUT = "NSNAGSRT";
 
 const MOCK_1ST_RETURN_EXAMPLE = 2;
 const MOCK_2ND_RETURN_EXAMPLE = 4;
@@ -23,6 +26,31 @@ describe("Ash", () => {
 		it(`should return ${MOCK_3RD_RETURN_EXAMPLE} as the third Premium Minds example`, () => {
 			const expectedPokemon = catchPokemon(MOCK_3RD_INPUT_EXAMPLE);
 			expect(expectedPokemon).toBe(2);
+		});
+
+		describe("testing error handlers", () => {
+			it(`should return ${MOCK_1ST_RETURN_EXAMPLE} with the second parameter`, () => {
+				const expectedPokemon = catchPokemon(MOCK_1ST_INPUT_EXAMPLE, false);
+				expect(expectedPokemon).toBe(2);
+			});
+
+			it(`should throw the error "${sysMessage.ERROR_NO_INPUT}..." for value empty value`, () => {
+				expect(() => {
+					catchPokemon("");
+				}).toThrow(Error);
+				expect(() => {
+					catchPokemon("");
+				}).toThrow(sysMessage.ERROR_NO_INPUT);
+			});
+
+			it(`should throw the error "${sysMessage.ERROR_INVALID_DIRECTIONS}..." for value empty value`, () => {
+				expect(() => {
+					catchPokemon(MOCK_INVALID_INPUT);
+				}).toThrow(Error);
+				expect(() => {
+					catchPokemon(MOCK_INVALID_INPUT);
+				}).toThrow(sysMessage.ERROR_INVALID_DIRECTIONS);
+			});
 		});
 	});
 });
